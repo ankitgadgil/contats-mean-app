8cb9fec731dc15b1ca1d493899fea652d09d50e0
Ankit <ankit>
2015-02-20 17:06:46 +0530

    adding index.html for base view

--- node-server.js
+++ node-server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var appPort = 4000;
 
-app.get('/', function (req, res) {
-	res.send("hello world from node-server.js")
-})
+app.use(express.static(__dirname + "/public"));
+
+// app.get('/', function (req, res) {
+// 	res.send("hello world from node-server.js")
+// })
 
 app.listen(appPort);
 console.log("server running on port "+appPort);
