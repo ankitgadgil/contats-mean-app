7c0479d802318bec43f7917d2424bf4c89b2e666
Ankit <ankit>
2015-02-23 13:47:53 +0530

    new superhero contact dynamically gets added

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,15 +1,23 @@
 function AppCtrl($scope, $http) {
 	console.log("Hello World from AppCtrl")
 
-	$http.get('/superherocontacts').success(function(response){
-		console.log("I got the data i requested");
-		$scope.superheros = response;
-	})
+	var refresh = function()
+	{
+		//this sends a REST request to the server for data gets a response from server
+		$http.get('/superherocontacts').success(function(response){
+			console.log("I got the data i requested");
+			$scope.superheros = response;
+			$scope.newsuperhero = {}; //clear input boxes
+		});
+	}
+
+	refresh();
 
 	$scope.addContact=function(){
 		console.log($scope.newsuperhero);
 		$http.post('/superherocontacts', $scope.newsuperhero).success(function(response){
 			console.log(response);
+			refresh();
 		});
 	}
 }
