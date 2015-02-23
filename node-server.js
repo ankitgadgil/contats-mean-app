b0cec6aac66905e6e9563135f6e2c72d8ecdc1c5
Ankit <ankit>
2015-02-23 11:57:54 +0530

    used a http get request for request->response->render model between server controller & view

--- node-server.js
+++ node-server.js
@@ -5,6 +5,31 @@ var appPort = 4000;
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/bower_components"));
 
+app.get('/contactlist', function (req, res) {
+	console.log("Received a GET request")
+
+	superhero1 = {
+		"name" : "Ironman",
+    	"email" : "ironman@avengers.org",
+    	"number" : "(A1) 111-1111"
+	};
+
+	superhero2 = {
+		"name" : "Hulk",
+	    "email" : "hulk@avengers.org",
+	    "number" : "(A2) 222-2222"
+	};
+	
+	superhero3 = {
+		"name" : "Thor",
+	    "email" : "thor@avengers.org",
+	    "number" : "(A3) 333-3333"
+	};
+	
+	var superheros = [superhero1, superhero2, superhero3]
+	res.json(superheros)
+})
+
 // app.get('/', function (req, res) {
 // 	res.send("hello world from node-server.js")
 // })
--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,24 +1,8 @@
-function AppCtrl($scope) {
+function AppCtrl($scope, $http) {
 	console.log("Hello World from AppCtrl")
 
-	superhero1 = {
-		"name" : "Ironman",
-    	"email" : "ironman@avengers.org",
-    	"number" : "(A1) 111-1111"
-	};
-
-	superhero2 = {
-		"name" : "Hulk",
-	    "email" : "hulk@avengers.org",
-	    "number" : "(A2) 222-2222"
-	};
-	
-	superhero3 = {
-		"name" : "Thor",
-	    "email" : "thor@avengers.org",
-	    "number" : "(A3) 333-3333"
-	};
-	
-	var superheros = { superhero1, superhero2, superhero3}
-	$scope.superheros = superheros;
+	$http.get('/contactlist').success(function(response){
+		console.log("I got the data i requested");
+		$scope.superheros = response;
+	})
 }
